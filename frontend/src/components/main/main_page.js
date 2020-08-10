1ef3acad12c394630ab6b2c73c387c9cf7b691b0
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-09 21:15:48 -0400

    Create main_page.js

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Twitter Clone</h1>
+        <footer>
+          Copywright &copy; 2020 Hootr
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
