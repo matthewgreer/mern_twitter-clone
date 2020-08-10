3498f30807b86768ccc8c6d3977e74fdd4a56571
matthewgreer <matthew.greer.dev@gmail.com>
2020-08-09 22:44:31 -0400

    Fix root_reducer , add signup action

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -24,6 +24,14 @@ export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT
 });
 
+export const signup = user => dispatch => (
+  APIUtil.signup(user)
+    .then(() => (
+      dispatch(receiveUserSignIn())
+    ), err => (
+      dispatch(receiveErrors(err.response.data))
+    ))
+);
 
 export const login = user => dispatch => (
   APIUtil.login(user)
